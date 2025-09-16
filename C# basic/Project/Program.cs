2609cf7316a8767e46b914431229ea60f9b54723
Xihao Yang <152677267+Xihao12@users.noreply.github.com>
2025-09-16 23:18:49 +0900

    Add files via upload

---
+++ C# basic/Project/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleCreatures
+{
+    class Program
+    {
+        static void Main()
+        {
+            // wrong comment:this is used for emoji
+            // correct comment:This program prints an ASCII art creature to the console
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            //wrong code:Console.WriteLine("| o |");
+            //correct one should be:
+            Console.WriteLine("| O |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("This is my creature");
+
+            // one more easy and consise way to print all the results below:
+            //Console.WriteLine(" .-.\n(o o)\n| O |\n|   |\n'~~~'");
+        }
+    }
+}
+
