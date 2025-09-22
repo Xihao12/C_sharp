c84d99d9291f79957d5106840d052f3e2523833e
Xihao Yang <152677267+Xihao12@users.noreply.github.com>
2025-09-22 23:27:49 +0900

    Add files via upload

---
+++ C# basic/Data Types and Variables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Form
+{
+    class Datatype
+    {
+        static void Main(string[] args)
+        {
+            //string is used for define variable that has character
+            string name = "Shadow";
+
+            string breed = "Golden Retriever";
+
+            int age = 5;
+
+            double weight = 65.22;
+
+            bool spayed = true;
+
+            Console.WriteLine(name);
+            Console.WriteLine(breed);
+            Console.WriteLine(age);
+            Console.WriteLine(weight);
+            Console.WriteLine(spayed);
+
+
+        }
+    }
+}
+
+
+
+
