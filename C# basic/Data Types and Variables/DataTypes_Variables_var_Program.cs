94b12830d43638b81c6a9be39cab6639204c8af4
Xihao Yang <152677267+Xihao12@users.noreply.github.com>
2025-09-22 23:34:22 +0900

    Add files via upload

---
+++ C# basic/Data Types and Variables/DataTypes_Variables_var_Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Form
+{
+    class Datatype
+    {
+        static void Main(string[] args)
+        {
+            var food = "Apples";
+            var age = 5;
+            var weight = 65.22;
+            var isActive = true;
+
+            Console.WriteLine(food);
+            Console.WriteLine(age);
+            Console.WriteLine(weight);
+            Console.WriteLine(isActive);
+        }
+    }
+}
+
+
+
+
